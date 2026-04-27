6979564708bd50eb54c9d4b2ea96b50fca1db5fb
Ally Atkins <87872480+atkinsa21@users.noreply.github.com>
2026-04-27 14:38:15 -0400

    Fix df.drop() call

--- src/clinicalxai/datasets.py
+++ src/clinicalxai/datasets.py
@@ -21,7 +21,7 @@ def load_diabetes_dataset():
         y_test (pd.Series): Testing target variable.
     """
     df = DF.copy()
-    X = df.drop(columns=[TARGET_COLUMN])
+    X = df.drop(columns=TARGET_COLUMN)
     y = df[TARGET_COLUMN]
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, stratify=y, test_size=0.2, random_state=42
